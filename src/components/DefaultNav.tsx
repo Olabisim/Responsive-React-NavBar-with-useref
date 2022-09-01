e4ea49ae9751869a0f47aa23b6776d7bdb4cc961
holabisii <ajoseholabisi@gmail.com>
2022-08-31 21:12:55 -0400

    created the default nav

---
+++ src/components/DefaultNav.tsx
@@ -0,0 +1,82 @@
+
+export const DefaultNav = () => {
+        return (
+                <div>
+                        
+                        <header className="h-20 nav-header mt-2 pt-0 p-10" style={{background: "black"}}>
+                                <nav className="relative px-0 lg:px-2 py-0">
+                                
+                                <div className="container mx-auto flex justify-between items-center">
+                                        <div className='flex'>
+
+                                                {/* <img src="../../Group.svg" alt="Tailwindcss Navigation" className='image-header' /> */}
+                                                <h2 className='Navlogo text-white text-xl lg:text-2xl font-thin mt-2 p-4 pt-2 tracking-wide' > PRODHIT</h2>
+                                        </div>
+
+                                        <ul className="hidden lg:flex space-x-12">
+                                                <li><a href="#ss" className='nav-lisa active-nav-lisa hover:tracking-widest' >Home</a></li>
+                                                <li><a href="#ss" className='nav-lisa hover:tracking-widest' >About Us</a></li>
+                                                <li className="flex relative group">
+                                                <a href="#ss" className="mr-1 nav-lisa hover:tracking-widest" >Our Team</a> 
+                                                <i className="fa-solid fa-chevron-down fa-2xs pt-3"></i>
+                                                {/* <!-- Submenu starts --> */}
+                                                <ul className="absolute bg-white p-3 w-52 top-6 transform scale-0 group-hover:scale-100 transition duration-150 ease-in-out origin-top shadow-lg">
+                                                        <li className="text-sm hover:bg-slate-100 leading-8 submenu_link p-4 hover:tracking-wider"><a href="#ss">Webdesign</a></li>
+                                                        <li className="text-sm hover:bg-slate-100 leading-8 submenu_link p-4 hover:tracking-wider"><a href="#ss">Digital marketing</a></li>
+                                                        <li className="text-sm hover:bg-slate-100 leading-8 submenu_link p-4 hover:tracking-wider"><a href="#ss">SEO</a></li>
+                                                        <li className="text-sm hover:bg-slate-100 leading-8 submenu_link p-4 hover:tracking-wider"><a href="#ss">Ad campaigns</a></li>
+                                                        <li className="text-sm hover:bg-slate-100 leading-8 submenu_link p-4 hover:tracking-wider"><a href="#ss">UX Design</a></li>
+                                                </ul>
+                                                {/* <!-- Submenu ends --> */}
+                                                </li>
+                                                <li><a href="#ss"  className='nav-lisa hover:tracking-widest'>Blog</a></li>
+                                        </ul>
+
+                                        
+                                        <div className='hidden lg:flex'>
+                                                <a href="#ss" className="Nav-button text-white m-2" role="button">
+                                                                Connect Wallet
+                                                </a>
+                                                {/* <img src="../../avatar.png " className="Nav-avatar mt-3" alt="Tailwindcss Navigat/ion" /> */}
+
+                                        </div>
+                                        
+                                        {/* <!-- Mobile menu icon --> */}
+                                                <div className='block lg:hidden mobile_nav_icon p-3'>
+                                                        <div></div>
+                                                        <div></div>
+                                                        <div></div>
+                                                </div>
+                                        </div>
+                                
+                                        {/* <!-- Mobile menu --> */}
+                                        <div className="lg:hidden flex justify-center mt-3 w-full">
+                                                <div id="mobile-menu" className="mobile-menu absolute top-23 w-full"> 
+                                                        {/* <!-- add hidden here later --> */}
+                                                        <ul className="main_menu_bg shadow-lg leading-9 font-bold h-screen">
+                                                                <li className="pl-4 main_menu_bg_li "><a href="#home" className="block pl-10">Home</a></li>
+                                                                <li className="pl-4 main_menu_bg_li "><a href="#news" className="block pl-10">News</a></li>
+                                                                <li className="main_menu_bg_li">
+                                                                <a href="#services" className="block pl-14">Services <i className="fa-solid fa-chevron-down fa-2xs pt-4"></i></a> 
+                                                                
+                                                                {/* <!-- Submenu starts --> */}
+                                                                <ul className="bg-white text-gray-800 w-full">
+                                                                        <li className="text-sm leading-8 font-normal hover:bg-slate-200 font-extrabold main_menu_bg_li_inner"><a className="block pl-16" href="#ss">Web design</a></li>
+                                                                        <li className="text-sm leading-8 font-normal hover:bg-slate-200 font-extrabold main_menu_bg_li_inner"><a className="block pl-16" href="#ss">Digital marketing</a></li>
+                                                                        <li className="text-sm leading-8 font-normal hover:bg-slate-200 font-extrabold main_menu_bg_li_inner"><a className="block pl-16" href="#ss">SEO</a></li>
+                                                                        <li className="text-sm leading-8 font-normal hover:bg-slate-200 font-extrabold main_menu_bg_li_inner"><a className="block pl-16" href="#ss">Ad campaigns</a></li>
+                                                                        <li className="text-sm leading-8 font-normal hover:bg-slate-200 font-extrabold main_menu_bg_li_inner"><a className="block pl-16" href="#ss">UX Design</a></li>
+                                                                </ul>
+                                                                {/* <!-- Submenu ends --> */}
+                                                                </li>
+                                                                <li className="pl-4 main_menu_bg_li "><a href="#about" className="block pl-10">About</a></li>
+                                                                <li className="pl-4 main_menu_bg_li "><a href="#contact" className="block pl-10">Contact</a></li>
+                                                        </ul>
+                                                </div>
+                                        </div>
+
+                                </nav>
+                        </header>
+                </div>
+        )
+}
